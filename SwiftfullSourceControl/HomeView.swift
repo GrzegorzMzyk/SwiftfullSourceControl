95bfa29ec2eca7ab6d4f14057d94bd11d523a5f7
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-06-09 18:00:21 +0200

    [Future] Add circle and rectengle

--- SwiftfullSourceControl/HomeView.swift
+++ SwiftfullSourceControl/HomeView.swift
@@ -12,12 +12,17 @@ struct HomeView: View {
     var body: some View {
         VStack {
             Text("press me 1")
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(width: 100, height: 100)
+            Text("Screen 1")
             Text("Screen 2")
             Text("Screen 3")
             Text("Screen 4")
             Image(systemName: "tv.circle")
                 .background(Color.blue)
                 .font(.title3)
+            Circle()
+                .frame(width: 100)
 
         }
     }
