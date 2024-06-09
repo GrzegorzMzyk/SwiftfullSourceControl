f426bdb60a698fda28d31ac1f8e6c6613296c34e
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-06-09 15:36:10 +0200

    Add Home View

---
+++ SwiftfullSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfullSourceControl
+//
+//  Created by Grzegorz Mzyk on 09/06/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
