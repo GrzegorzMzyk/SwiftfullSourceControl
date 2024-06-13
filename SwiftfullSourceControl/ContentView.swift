1b275acf07a27f0cd6275830115691d2e08d680c
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-06-13 20:32:59 +0200

    Another commit

--- SwiftfullSourceControl/ContentView.swift
+++ SwiftfullSourceControl/ContentView.swift
@@ -57,7 +57,7 @@ struct ContentView: View {
                 .padding()
                 .background(Color.red)
         })
-        Button("seconf button") {
+        Button("Button") {
             
         }
             
