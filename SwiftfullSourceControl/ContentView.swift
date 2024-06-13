459fab41026cee28a472708bf5c1fffb611d3d29
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-06-13 12:36:04 +0200

    [Feature] Second Button

--- SwiftfullSourceControl/ContentView.swift
+++ SwiftfullSourceControl/ContentView.swift
@@ -56,6 +56,9 @@ struct ContentView: View {
                 .padding()
                 .background(Color.red)
         })
+        Button("seconf button") {
+            
+        }
             
         }
     
