e31e8f245749e4c41efb9b70f92e40e5424eedfd
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-06-13 11:34:29 +0200

    xxxx

--- SwiftfullSourceControl/HomeView.swift
+++ SwiftfullSourceControl/HomeView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = "mmm"
+    @State private var title: String = "mmmm"
     var body: some View {
         VStack {
             Text("press me 10")
