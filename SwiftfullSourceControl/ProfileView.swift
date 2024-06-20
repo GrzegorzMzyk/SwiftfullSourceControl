b335c9b3c0adc3b2822b2d8d88b7dfe4bba6740c
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-06-20 13:00:14 +0200

    [Feature] adding profile view

---
+++ SwiftfullSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfullSourceControl
+//
+//  Created by Grzegorz Mzyk on 20/06/2024.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello, Grzegorz!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
